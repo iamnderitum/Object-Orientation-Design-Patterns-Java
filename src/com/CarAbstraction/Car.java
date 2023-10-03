953ff7e6aa41de1f806939da94ab66c579d30d6d
iamnderitum <iamnderitum@gmail.com>
2023-10-03 17:04:20 +0300

    Setup for Car Abstraction - Main Methods

---
+++ src/com/CarAbstraction/Car.java
@@ -0,0 +1,24 @@
+package com.CarAbstraction;
+
+public class Car {
+
+    String manufacturer;
+    float fuel;
+    int registrationNum;
+
+    void SwitchOn(){
+
+    }
+
+    void switchOff(){
+
+    }
+
+    void Accelerate(){
+
+    }
+
+    void Brake(){
+
+    }
+}
