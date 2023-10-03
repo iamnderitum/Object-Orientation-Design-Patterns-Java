c0d06d0733916baec69c0e812be43422324a9e5b
iamnderitum <iamnderitum@gmail.com>
2023-10-03 17:20:14 +0300

    Adding Encapsulation and Main program - Denote car Identifiers as Private - make sure they are only accessible via the class

--- src/com/CarAbstraction/Car.java
+++ src/com/CarAbstraction/Car.java
@@ -2,23 +2,34 @@ package com.CarAbstraction;
 
 public class Car {
 
-    String manufacturer;
-    float fuel;
-    int registrationNum;
+    private String manufacturer;
+    private float fuel;
+    private float speed;
+    private int registrationNum;
+    private boolean isRunning;
 
     void SwitchOn(){
-
+        isRunning = true;
     }
 
     void switchOff(){
-
+        isRunning = false;
     }
 
     void Accelerate(){
+        if(!isRunning)
+            return ;
 
+        speed++;
+        fuel -= 1.5f ;
     }
 
     void Brake(){
+        speed = 0 ;
+    }
 
+    void Dashboard(){
+        System.out.println("speed: " + speed);
+        System.out.println("fuel: " + fuel);
     }
 }
---
+++ src/com/CarAbstraction/Program.java
@@ -0,0 +1,13 @@
+package com.CarAbstraction;
+
+public class Program {
+
+    public static void main(String[] args){
+        Car car = new Car() ;
+        car.SwitchOn();
+        car.Accelerate();
+        car.Accelerate();
+        car.Accelerate();
+        car.Dashboard();
+    }
+}
